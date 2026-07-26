7a7f4a1f4c711e196f99960e7958319ea3553cdf
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-26 00:21:53 +0000

    适配 SunnyPT 站点：Bearer token 走 api.sunnypt.top 用户信息接口保活

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-07-24 11:51:24"
-ts = 1784893884
+ver = "2026-07-26 00:21:53"
+ts = 1785025313
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
