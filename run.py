99343201191dd426e12374d304c5ce23eeb7f83b
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-27 06:35:40 +0000

    fix: self引用错误导致编译报错，CI编译异常应抛出终止流程

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-07-27 00:02:29"
-ts = 1785081749
+ver = "2026-07-27 14:35:39"
+ts = 1785134139
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
