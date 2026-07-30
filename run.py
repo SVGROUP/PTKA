d377b69b6cb21b7935dd9a978e4206583548d353
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-30 13:48:17 +0000

    修复登录卡死占死调度名额：requests 加真超时 + 随机登录加超时兜底

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-07-30 21:36:27"
-ts = 1785418587
+ver = "2026-07-30 21:48:17"
+ts = 1785419297
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
