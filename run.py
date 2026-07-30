9a03e9447cfc7cd1c375132f70244f61e9aa1160
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-30 09:03:41 +0000

    修复镜像升级通知未展示同 hash 具名版本 tag 的问题

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-07-28 15:29:07"
-ts = 1785223747
+ver = "2026-07-30 17:03:41"
+ts = 1785402221
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
