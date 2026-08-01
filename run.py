6a59fd3d21ad457f1def0d4af6e3a40ca4d4b48b
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-01 15:11:25 +0000

    feat(upgrade): 待升级列表加删除模式开关，避免误删

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-08-01 23:06:53"
-ts = 1785596813
+ver = "2026-08-01 23:11:25"
+ts = 1785597085
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
