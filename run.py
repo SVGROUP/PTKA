f8663c2df3d29a081758337094f60563ed261600
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-08 03:24:55 +0000

    修复config页面卡片删除按钮缺失样式

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-08-08 11:12:32"
-ts = 1786158752
+ver = "2026-08-08 11:24:55"
+ts = 1786159495
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
