030b625f8e0835d5553a778147dc31efc59c83d1
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-11 16:54:34 +0000

    fix(check): 本地无 RepoDigest 的镜像也能正常入 image_updates

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 from pt_keepalive.app import main
-ver = "2026-08-08 13:08:31"
-ts = 1786165711
+ver = "2026-08-12 00:54:34"
+ts = 1786467274
 if __name__ == "__main__":
     os.environ["PTKA_VERSION"] = ver
     os.environ["PTKA_BUILD_TS"] = str(ts)
